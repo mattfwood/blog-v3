e93066c1ea978a5b1a838927a3fc1cb87021d015
Matt Wood <matt@cratebind.com>
2020-09-30 09:09:41 -0500

    Remove unneeded fields in generate blog post script

--- scripts/generate-blogpost.js
+++ scripts/generate-blogpost.js
@@ -65,7 +65,6 @@ async function generateBlogPost() {
       slug,
       title,
       date: formatDate(new Date()),
-      // author: 'Kent C. Dodds',
       description: `_${description}_`,
       categories: listify(categories),
       keywords: listify(keywords),
